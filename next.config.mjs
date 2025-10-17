a61deeb48e909613621a03a014473f8aee596f38
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-10-17 20:02:47 +0600

    chore: update API_URL to new backend endpoint

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   env: {
     // API_URL: "http://localhost:8000",
     // API_URL: "https://finance-api.codesharer.xyz",
-    API_URL: "https://finance.mainulhasan99.xyz",
+    API_URL: "https://expense-tracker-backend-l4ls.onrender.com",
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   },
   images: {
