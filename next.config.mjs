804bed61c3985cab0ee01562fdd3d7e5dc6405f0
Mainulhasan05 <mdrifatbd5@gmail.com>
2025-10-31 12:26:13 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://localhost:8000",
+    // API_URL: "http://localhost:8000",
     // API_URL: "https://finance-api.codesharer.xyz",
-    // API_URL: "https://expense-api.chapai.info",
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    API_URL: "https://expense-api.chapai.info",
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: "600625956491-fr06e8jgoe2u9ce1v2e664p3oj62dgsr.apps.googleusercontent.com",
   },
   images: {
     domains: ["lh3.googleusercontent.com"],
